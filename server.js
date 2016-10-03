55d84bd739508dad4ed97970ff0baac7b6a955dc
PKief <philipp.kief@gmx.de>
2016-10-03 12:59:05 +0200

    updated package.json updated running ports: dev: 3000 and prod: 9000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-var PORT = process.env.PORT || 8001;
+var PORT = process.env.PORT || 9000;
 app.listen(PORT, function () {
     console.log('Production Express server running at localhost:' + PORT);
 });
