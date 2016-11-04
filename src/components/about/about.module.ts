1d461b3e86c1182d43b1cb9e4cf809a31f729aef
PKief <philipp.kief@gmx.de>
2016-11-04 09:10:01 +0100

    removed unused import

--- src/components/about/about.module.ts
+++ src/components/about/about.module.ts
@@ -1,5 +1,4 @@
 import { NgModule, Component } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about.component';
 import { routing } from './about.routing';
 
