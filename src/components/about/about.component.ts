ee28ce5d6626432e753b69977ab270eef8fadc68
PKief <philipp.kief@gmx.de>
2016-11-08 21:25:32 +0100

    go with params

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'about',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
         <h1>I am about</h1>
     `
 })
-export class AboutComponent { }
+export class AboutComponent {
+    params: string[];
+
+    constructor(private route: ActivatedRoute) {
+        this.route.params.subscribe(p => {
+            console.log(p);
+        });
+    }
+}
--- src/components/welcome/welcome.component.ts
+++ src/components/welcome/welcome.component.ts
@@ -6,13 +6,17 @@ import { Component } from '@angular/core';
     templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {
-    constructor(private router: Router){}
+    constructor(private router: Router) { }
 
     showPopups() {
         this.router.navigate(['/app/welcome', { outlets: { 'popup': 'message', 'popup2': 'message2' } }]);
-    }    
+    }
 
-    hidePopups(){
-        this.router.navigate(['/app/welcome', {outlets: {'popup': null, 'popup2': null }}]);
+    hidePopups() {
+        this.router.navigate(['/app/welcome', { outlets: { 'popup': null, 'popup2': null } }]);
+    }
+
+    goToAboutWithParams() {
+        this.router.navigate(['/app/about', { dataForm: 'test', page: '2' }]);
     }
 }
