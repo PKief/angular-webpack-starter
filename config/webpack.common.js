4f2ea1f8a32840e440ee459e81df39bfdf2458e2
PKief <philipp.kief@gmx.de>
2017-04-04 08:43:01 +0200

    angular 4

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
+var path = require('path');
 
 module.exports = {
     entry: {
@@ -53,10 +54,8 @@ module.exports = {
     plugins: [
         // Workaround for angular/angular#11580
         new webpack.ContextReplacementPlugin(
-            // The (\\|\/) piece accounts for path separators in *nix and Windows
-            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-            helpers.root('./src'), // location of your src
-            {} // a map of your routes
+            /angular(\\|\/)core(\\|\/)@angular/,
+            path.resolve(__dirname, '../src')
         ),
 
         new webpack.optimize.CommonsChunkPlugin({
